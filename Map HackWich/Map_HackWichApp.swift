4f1045737e0d4cfc8d6b63e73b308adff3760803
Carson Payne <carson.j.payne14@gmail.com>
2025-02-03 09:41:10 -0600

    Initial Commit

---
+++ Map HackWich/Map_HackWichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackWichApp.swift
+//  Map HackWich
+//
+//  Created by Carson Payne on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackWichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
